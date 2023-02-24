0e7ef3bf03d99d71824bcbeab87cd1ef3e04818e
Alex chow <zhouchuanglin@gmail.com>
2023-02-24 17:04:03 +0100

    添加 <用户表>只显示某Id 和 显示一组Id 例子

--- Demo/ConsoleApp1/Program.cs
+++ Demo/ConsoleApp1/Program.cs
@@ -17,30 +17,30 @@ var fsql = new FreeSqlBuilder().UseConnectionString(FreeSql.DataType.Sqlite, "Da
          .UseMonitorCommand(cmd => System.Console.WriteLine(cmd.CommandText + Environment.NewLine))
          .Build();
 
-List<AspNetUsers>? users=new List<AspNetUsers>();
+List<AspNetUsers>? users = new List<AspNetUsers>();
 
-//列出用户带角色名称
-users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeMany(a=>a.AspNetUserRoless,then =>then.Include(c=>c.AspNetRoless)).ToList();
-users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n" ));
+////列出用户带角色名称
+//users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeMany(a=>a.AspNetUserRoless,then =>then.Include(c=>c.AspNetRoless)).ToList();
+//users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n" ));
 
-users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeMany(a => a.AspNetUserRoless, then => then.IncludeByPropertyName(nameof(AspNetUserRoles.AspNetRoless))).ToList();
-users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
+//users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeMany(a => a.AspNetUserRoless, then => then.IncludeByPropertyName(nameof(AspNetUserRoles.AspNetRoless))).ToList();
+//users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
 
-users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeByPropertyName(nameof(AspNetUsers.AspNetUserRoless),then => then.IncludeByPropertyName(nameof(AspNetUserRoles.AspNetRoless))).ToList();
-users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
+//users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeByPropertyName(nameof(AspNetUsers.AspNetUserRoless),then => then.IncludeByPropertyName(nameof(AspNetUserRoles.AspNetRoless))).ToList();
+//users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
 
-//列出Admin的用户
+////列出Admin的用户
 users = fsql.Select<AspNetUsers>().IncludeMany(a => a.AspNetUserRoless, then => then.Include(c => c.AspNetRoless).Where (d=>d.AspNetRoless.Name== "Admin")).ToList();
 users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
 
-//var users3 = fsql.Select<AspNetUsers>().IncludeMany(a => a.AspNetUserRoless, then => then.Include(c => c.AspNetRoless)).Where (d=>d.AspNetUserRoless.First().RoleName == "Admin").ToSql ();
+////var users3 = fsql.Select<AspNetUsers>().IncludeMany(a => a.AspNetUserRoless, then => then.Include(c => c.AspNetRoless)).Where (d=>d.AspNetUserRoless.First().RoleName == "Admin").ToSql ();
 
-Expression<Func<AspNetUserRoles, bool>>? where = null;
+//Expression<Func<AspNetUserRoles, bool>>? where = null;
 
-where = where.And(d => d.AspNetRoless.Name == "Admin"); 
+//where = where.And(d => d.AspNetRoless.Name == "Admin"); 
 
-var dywhere = new DynamicFilterInfo { Field = "AspNetRoless.Name", Operator = DynamicFilterOperator.Equal, Value = "Admin" };
+//var dywhere = new DynamicFilterInfo { Field = "AspNetRoless.Name", Operator = DynamicFilterOperator.Equal, Value = "Admin" };
 
-var users4 = fsql.Select<AspNetUsers>().IncludeByPropertyName("AspNetUserRoless", then => then.WhereDynamicFilter(dywhere)).ToList();
+//users = fsql.Select<AspNetUsers>().IncludeByPropertyName("AspNetUserRoless", then => then.WhereDynamicFilter(dywhere)).Where(a => a.AspNetUserRoless.Any()).ToList();
 
-users4.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
+//users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
