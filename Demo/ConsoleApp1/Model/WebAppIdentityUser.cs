4bec099ef045533e4b2e52553a89e99f69182bbf
Alex chow <zhouchuanglin@gmail.com>
2023-02-24 16:26:50 +0100

    添加测试console

---
+++ Demo/ConsoleApp1/Model/WebAppIdentityUser.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Densen.Identity.Models;
+
+public class WebAppIdentityUser : IdentityUser
+{
+
+
+    /// <summary>
+    /// Full name
+    /// </summary>
+    [Display(Name = "全名")]
+    [PersonalData]
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Birth Date
+    /// </summary>
+    [Display(Name = "生日")]
+    [PersonalData]
+    public DateTime? DOB { get; set; }
+
+    [Display(Name = "识别码")]
+    public string? UUID { get; set; }
+
+    [Display(Name = "外联")]
+    public string? provider { get; set; }
+
+    [Display(Name = "税号")]
+    [PersonalData]
+    public string? TaxNumber { get; set; }
+
+    [Display(Name = "街道地址")]
+    [PersonalData]
+    public string? Street { get; set; }
+
+    [Display(Name = "邮编")]
+    [PersonalData]
+    public string? Zip { get; set; }
+
+    [Display(Name = "县")]
+    [PersonalData]
+    public string? County { get; set; }
+
+    [Display(Name = "城市")]
+    [PersonalData]
+    public string? City { get; set; }
+
+    [Display(Name = "省份")]
+    [PersonalData]
+    public string? Province { get; set; }
+
+    [Display(Name = "国家")]
+    [PersonalData]
+    public string? Country { get; set; }
+
+    [Display(Name = "类型")]
+    [PersonalData]
+    public string? UserRole { get; set; }
+}
---
+++ Demo/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using Densen.Models.ids;
+using FreeSql;
+using FreeSql.Internal.Model;
+using System.Linq.Expressions;
+
+var fsql = new FreeSqlBuilder().UseConnectionString(FreeSql.DataType.Sqlite, "Data Source=../../../../BlazorApp1/demo.db;")
+         //开发环境:自动同步实体
+         .UseAutoSyncStructure(true)
+         .UseNoneCommandParameter(true)
+         //调试sql语句输出
+         .UseMonitorCommand(cmd => System.Console.WriteLine(cmd.CommandText + Environment.NewLine))
+         .Build();
+
+List<AspNetUsers>? users=new List<AspNetUsers>();
+
+//列出用户带角色名称
+users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeMany(a=>a.AspNetUserRoless,then =>then.Include(c=>c.AspNetRoless)).ToList();
+users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n" ));
+
+users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeMany(a => a.AspNetUserRoless, then => then.IncludeByPropertyName(nameof(AspNetUserRoles.AspNetRoless))).ToList();
+users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
+
+users = fsql.Select<AspNetUsers>().Where(a => a.Email == "test@test.com").IncludeByPropertyName(nameof(AspNetUsers.AspNetUserRoless),then => then.IncludeByPropertyName(nameof(AspNetUserRoles.AspNetRoless))).ToList();
+users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
+
+//列出Admin的用户
+users = fsql.Select<AspNetUsers>().IncludeMany(a => a.AspNetUserRoless, then => then.Include(c => c.AspNetRoless).Where (d=>d.AspNetRoless.Name== "Admin")).ToList();
+users.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
+
+//var users3 = fsql.Select<AspNetUsers>().IncludeMany(a => a.AspNetUserRoless, then => then.Include(c => c.AspNetRoless)).Where (d=>d.AspNetUserRoless.First().RoleName == "Admin").ToSql ();
+
+Expression<Func<AspNetUserRoles, bool>>? where = null;
+
+where = where.And(d => d.AspNetRoless.Name == "Admin"); 
+
+var dywhere = new DynamicFilterInfo { Field = "AspNetRoless.Name", Operator = DynamicFilterOperator.Equal, Value = "Admin" };
+
+var users4 = fsql.Select<AspNetUsers>().IncludeByPropertyName("AspNetUserRoless", then => then.WhereDynamicFilter(dywhere)).ToList();
+
+users4.ForEach(a => System.Console.WriteLine($"\n用户 {a.Email} 角色名称 {a.RoleName}\n"));
