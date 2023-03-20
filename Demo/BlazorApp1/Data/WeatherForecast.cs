52eeb983251f1078c53bc4ac0fbfaffe80209a09
Alex chow <zhouchuanglin@gmail.com>
2023-03-20 12:04:38 +0100

    更新例子

--- Demo/BlazorApp1/Data/WeatherForecast.cs
+++ Demo/BlazorApp1/Data/WeatherForecast.cs
@@ -1,19 +1,34 @@
-// ********************************** 
-// Densen Informatica 中讯科技 
-// 作者：Alex Chow
-// e-mail:zhouchuanglin@gmail.com 
-// **********************************
+﻿using BootstrapBlazor.Components;
+using FreeSql.DataAnnotations;
+using Magicodes.ExporterAndImporter.Excel;
+using OfficeOpenXml.Table;
+using System.ComponentModel;
 
-namespace BlazorApp1.Data
+namespace BlazorApp1.Data;
+
+[ExcelImporter(IsLabelingError = true)]
+[ExcelExporter(Name = "导入商品中间表", TableStyle = TableStyles.Light10, AutoFitAllColumn = true)]
+[AutoGenerateClass(Searchable = true, Filterable = true, Sortable = true)]
+public class WeatherForecast
 {
-    public class WeatherForecast
-    {
-        public DateOnly Date { get; set; }
+    [Column(IsIdentity = true)]
+    [DisplayName("序号")]
+    public int ID { get; set; }
 
-        public int TemperatureC { get; set; }
+    [DisplayName("日期")]
+    public DateTime Date { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureC { get; set; }
 
-        public string? Summary { get; set; }
-    }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+
+    //[DisplayName("备注")]
+    //public string? Remarks { get; set; }
+
+    [DisplayName("备注表")]
+    [Navigate ("ID")]
+    public virtual  Remarks? Remark { get; set; }
+    //在 本实体 查找 ID 属性，与 Remarks 主键 关联
 }
--- Demo/BlazorApp1/Data/WeatherForecastService.cs
+++ Demo/BlazorApp1/Data/WeatherForecastService.cs
@@ -1,26 +1,20 @@
-// ********************************** 
-// Densen Informatica 中讯科技 
-// 作者：Alex Chow
-// e-mail:zhouchuanglin@gmail.com 
-// **********************************
+﻿namespace BlazorApp1.Data;
 
-namespace BlazorApp1.Data
+public class WeatherForecastService
 {
-    public class WeatherForecastService
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+    public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+    {
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
-        }
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            Remark= new Remarks {Remark = "Remark" + index }
+        }).ToArray());
     }
 }
