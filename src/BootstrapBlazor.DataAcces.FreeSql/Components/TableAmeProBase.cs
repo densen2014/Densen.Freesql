7ed7791abaaddbcf3ac462fa38c2e7fe7307c248
Alex chow <zhouchuanglin@gmail.com>
2024-02-03 15:43:06 +0100

    动态生成控件 TableColumn 明细行按钮

--- src/BootstrapBlazor.DataAcces.FreeSql/Components/TableAmeProBase.cs
+++ src/BootstrapBlazor.DataAcces.FreeSql/Components/TableAmeProBase.cs
@@ -545,28 +545,18 @@ public partial class TableAmeProBase<TItem> : TableAmeBase where TItem : class, 
     {
         var fieldExpresson = Utility.GenerateValueExpression(model, Field ?? FieldD ?? "ID", FieldType);
         var typeTableColumn = typeof(TableColumn<,>).MakeGenericType(typeof(TItem), FieldType);
-        var value = Utility.GetPropertyValue(model, Field ?? FieldD ?? "ID");
-        //if (value == null)
-        //{
-        //    return;
-        //}
-        //var valueType=value.GetType();
         builder.OpenComponent(0, typeTableColumn);
         builder.AddAttribute(1, "FieldExpression", fieldExpresson);
-        //var typeTableColumnContext = typeof(TableColumnContext<,>).MakeGenericType(typeof(TItem), FieldType);
         builder.AddAttribute(2, "Template", DialogTableDetails(FieldType));
-        //if (Type.GetTypeCode(FieldType) == TypeCode.Int32)
-        //{
-        //    builder.AddAttribute(2, "Template", DialogTableDetails<int>());
-        //}
-        //else
-        //{
-        //    builder.AddAttribute(2, "Template", DialogTableDetails<string>());
-        //}
         builder.AddAttribute(3, "Visible", true);
         builder.CloseComponent();
     };
 
+    /// <summary>
+    /// 动态生成控件 TableColumn 明细行按钮
+    /// </summary>
+    /// <param name="fieldType"></param>
+    /// <returns></returns>
     private object DialogTableDetails(Type fieldType)
     {
         var valueChangedInvoker = CreateLambda(this, fieldType).Compile();
@@ -644,17 +634,8 @@ public partial class TableAmeProBase<TItem> : TableAmeBase where TItem : class, 
         var valueType = value.GetType();
         builder.OpenComponent(0, typeof(TableColumn<,>).MakeGenericType(typeof(TItem), FieldType));
         builder.AddAttribute(1, "FieldExpression", fieldExpresson);
-        //builder.AddAttribute(2, "Template", DialogTableDetails(valueType, value));
-
-        //// 添加模板
-        //if (Type.GetTypeCode(FieldType) == TypeCode.Int32)
-        //{
-        //    builder.AddAttribute(2, "Template", DialogTableDetails<int>());
-        //}
-        //else
-        //{
-        //    builder.AddAttribute(2, "Template", DialogTableDetails<string>());
-        //}
+        // 添加模板
+        builder.AddAttribute(2, "Template", DialogTableDetails(FieldType));
         builder.CloseComponent();
     };
 
