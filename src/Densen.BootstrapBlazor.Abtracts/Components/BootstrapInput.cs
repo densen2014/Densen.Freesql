41043ca91a0f485850dcffad733f2c535fa9d279
Alex chow <zhouchuanglin@gmail.com>
2024-12-28 19:05:38 +0100

    导入类型

---
+++ src/Densen.BootstrapBlazor.Abtracts/Components/BootstrapInput.cs
@@ -0,0 +1,133 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+#if NET20_OR_GREATER || NETSTANDARD2_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace BootstrapBlazor.Components;
+
+
+/// <summary>
+/// BootstrapInput 组件
+/// </summary>
+public partial class BootstrapInput<TValue>
+{
+}
+
+/// <summary>
+/// 可为空布尔值组件
+/// </summary>
+public partial class NullSwitch
+{
+}
+
+/// <summary>
+/// DateTimePicker 组件
+/// </summary>
+public partial class DateTimePicker<TValue>
+{
+}
+
+/// <summary>
+/// An input component for editing numeric values.
+/// Supported numeric types are <see cref="int"/>, <see cref="long"/>, <see cref="short"/>, <see cref="float"/>, <see cref="double"/>, <see cref="decimal"/>.
+/// </summary>
+public partial class BootstrapInputNumber<TValue>
+{
+}
+/// <summary>
+/// Select 组件实现类
+/// </summary>
+/// <typeparam name="TValue"></typeparam>
+public partial class Select<TValue> : ISelect, ILookup
+{
+    IEnumerable<SelectedItem>? ILookup.Lookup { get; set; }
+    StringComparison ILookup.LookupStringComparison { get; set; }
+    string? ILookup.LookupServiceKey { get; set; }
+    object? ILookup.LookupServiceData { get; set; }
+    ILookupService? ILookup.LookupService { get; set; }
+
+    public void Add(SelectedItem item)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+/// <summary>
+/// ISelect 接口
+/// </summary>
+public interface ISelect
+{
+    /// <summary>
+    /// 增加 SelectedItem 项方法
+    /// </summary>
+    /// <param name="item"></param>
+    void Add(SelectedItem item);
+}
+/// <summary>
+/// Lookup 接口定义
+/// </summary>
+public interface ILookup
+{
+    /// <summary>
+    /// 获得/设置 数据集用于 CheckboxList Select 组件 通过 Value 显示 Text 使用 默认 null
+    /// <para>设置 <see cref="Lookup"/> 参收后，<see cref="LookupServiceKey"/> 和 <see cref="LookupServiceData"/> 两个参数均失效</para>
+    /// </summary>
+    IEnumerable<SelectedItem>? Lookup { get; set; }
+
+    /// <summary>
+    /// 获得/设置 字典数据源字符串比较规则 默认 <see cref="StringComparison.OrdinalIgnoreCase" /> 大小写不敏感 
+    /// </summary>
+    StringComparison LookupStringComparison { get; set; }
+
+    /// <summary>
+    /// 获得/设置 <see cref="ILookupService"/> 服务获取 Lookup 数据集合键值 常用于外键自动转换为名称操作，可以通过 <see cref="LookupServiceData"/> 传递自定义数据
+    /// <para>未设置 <see cref="Lookup"/> 时生效</para>
+    /// </summary>
+    string? LookupServiceKey { get; set; }
+
+    /// <summary>
+    /// 获得/设置 <see cref="ILookupService"/> 服务获取 Lookup 数据集合键值自定义数据，通过 <see cref="LookupServiceKey"/> 指定键值
+    /// <para>未设置 <see cref="Lookup"/> 时生效</para>
+    /// </summary>
+    object? LookupServiceData { get; set; }
+
+    /// <summary>
+    /// 获得/设置 <see cref="ILookupService"/> 服务实例
+    /// </summary>
+    ILookupService? LookupService { get; set; }
+}
+
+/// <summary>
+/// ILookupService 接口
+/// </summary>
+public interface ILookupService
+{
+    /// <summary>
+    /// 根据指定键值获取 Lookup 集合方法
+    /// </summary>
+    /// <param name="key">获得 Lookup 数据集合键值</param>
+    [Obsolete("已弃用，请使用 data 参数重载方法；Deprecated, please use the data parameter method")]
+    [ExcludeFromCodeCoverage]
+    IEnumerable<SelectedItem>? GetItemsByKey(string? key);
+
+    /// <summary>
+    /// 根据指定键值获取 Lookup 集合方法
+    /// </summary>
+    /// <param name="key">获得 Lookup 数据集合键值</param>
+    /// <param name="data">Lookup 键值附加数据</param>
+    IEnumerable<SelectedItem>? GetItemsByKey(string? key, object? data);
+
+    /// <summary>
+    /// 根据指定键值获取 Lookup 集合异步方法
+    /// </summary>
+    /// <param name="key">获得 Lookup 数据集合键值</param>
+    /// <param name="data">Lookup 键值附加数据</param>
+    Task<IEnumerable<SelectedItem>?> GetItemsByKeyAsync(string? key, object? data);
+}
+#endif
---
+++ src/Densen.BootstrapBlazor.Abtracts/Utils/Utility.cs
@@ -0,0 +1,116 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+#if NET20_OR_GREATER || NETSTANDARD2_0_OR_GREATER 
+using System.ComponentModel;
+using System.Data;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Utility 帮助类
+/// </summary>
+public static class Utility
+{
+    /// <summary>
+    /// 判断当前 <see cref="ILookup"/> 实例是否配置 Lookup 数据
+    /// </summary>
+    /// <param name="lookup"></param>
+    /// <returns></returns>
+    public static bool IsLookup(this ILookup lookup) => lookup.Lookup != null || !string.IsNullOrEmpty(lookup.LookupServiceKey);
+
+    /// <summary>
+    /// 检查是否为 Number 数据类型
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static bool IsNumber(this Type t)
+    {
+        var targetType = Nullable.GetUnderlyingType(t) ?? t;
+        return targetType == typeof(int) || targetType == typeof(long) || targetType == typeof(short) ||
+            targetType == typeof(float) || targetType == typeof(double) || targetType == typeof(decimal);
+    }
+
+    /// <summary>
+    /// 检查是否为 Boolean 数据类型
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static bool IsBoolean(this Type t)
+    {
+        var targetType = Nullable.GetUnderlyingType(t) ?? t;
+        return targetType == typeof(Boolean);
+    }
+
+    /// <summary>
+    /// 检查是否为 DateTime 数据类型
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static bool IsDateTime(this Type t)
+    {
+        var targetType = Nullable.GetUnderlyingType(t) ?? t;
+        var check = targetType == typeof(DateTime) || targetType == typeof(DateTimeOffset);
+        return check;
+    }
+
+    /// <summary>
+    /// 检查是否为 TimeSpan 数据类型
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static bool IsTimeSpan(this Type t)
+    {
+        var targetType = Nullable.GetUnderlyingType(t) ?? t;
+        var check = targetType == typeof(TimeSpan);
+        return check;
+    }
+
+    /// <summary>
+    /// 获得 当前输入语言小数点分隔符
+    /// </summary>
+    private static string NumberDecimalSeparator => CultureInfo.CurrentCulture?.NumberFormat?.NumberDecimalSeparator ?? ".";
+
+    /// <summary>
+    /// 通过指定类型生成组件类型
+    /// </summary>
+    /// <param name="item"></param>
+    public static Type GenerateComponentType(IEditorItem item)
+    {
+        var fieldType = item.PropertyType;
+        Type? ret = null;
+        var type = (Nullable.GetUnderlyingType(fieldType) ?? fieldType);
+        if (type.IsEnum)
+        {
+            ret = typeof(Select<>).MakeGenericType(fieldType);
+        }
+        else if (fieldType == typeof(bool?))
+        {
+            ret = typeof(NullSwitch);
+        }
+        else if (fieldType.IsNumber() && NumberDecimalSeparator == ".")
+        {
+            ret = typeof(BootstrapInputNumber<>).MakeGenericType(fieldType);
+        }
+        else if (fieldType.IsDateTime())
+        {
+            ret = typeof(DateTimePicker<>).MakeGenericType(fieldType);
+        }
+        else if (fieldType.IsBoolean())
+        {
+            ret = typeof(Switch);
+        }
+        else if (fieldType == typeof(string))
+        {
+            ret = item.Rows > 0 ? typeof(Textarea) : typeof(BootstrapInput<>).MakeGenericType(typeof(string));
+        }
+        return ret ?? typeof(BootstrapInput<>).MakeGenericType(fieldType);
+    }
+}
+#endif
